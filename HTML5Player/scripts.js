b70465f2227011753c86b008220846cff5e68866
Tianyu Duan <tianyuduan1991@gmail.com>
2017-12-13 01:25:59 -0800

    finished Custom HTML player

--- HTML5Player/scripts.js
+++ HTML5Player/scripts.js
@@ -25,10 +25,49 @@ function togglePlay(){
 function updatePlayButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   toggle.textContent = icon;
+}
+
+
+function skip(){
+  console.log("im skipping");
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+  console.log(this.value);
+  video[this.name ] = this.value;
+}
+
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 
 }
+
+function scrub(e){
+  console.log(e);
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 // hook up event listenrs
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updatePlayButton);
 video.addEventListener('pause', updatePlayButton);
 toggle.addEventListener('click', togglePlay);
+toggle.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => {
+  mousedown = true;
+});
+progress.addEventListener('mouseup', () => {
+  mousedown = false;
+});
