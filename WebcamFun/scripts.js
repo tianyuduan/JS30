d9c049503c7fb819f4fa1e88499f71880f84e3bb
Tianyu Duan <tianyuduan1991@gmail.com>
2018-01-06 13:38:24 -0800

    creating countdown timer

--- WebcamFun/scripts.js
+++ WebcamFun/scripts.js
@@ -53,7 +53,7 @@ function paintToCanvas() {
   function rgbSplit(pixels) {
     for(let i = 0; i < pixels.data.length; i+=4) {
       pixels.data[i - 150] = pixels.data[i + 0]; // RED
-      pixels.data[i + 500] = pixel s.data[i + 1]; // GREEN
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
       pixels.data[i - 550] = pixels.data[i + 2]; // Blue
     }
     return pixels;
